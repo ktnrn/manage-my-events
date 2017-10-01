daae60c37068607692f2453127b87f36f8d7d6cc
ktnrn <java.lang.k10exception@gmail.com>
2017-10-01 15:55:53 -0700

    Added http-server package. Added app skeleton with index.html, app.js and dashboardController.js Linked all js in index.html

---
+++ .history/app/src/dashboardController_20171001154113.js
@@ -0,0 +1,21 @@
+var app = angular.module("app", ['ngMaterial']);
+
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('dashboardController', dashboardController);
+
+    dashboardController.$inject = [];
+    function dashboardController() {
+        var vm = this;
+        
+
+        activate();
+
+        ////////////////
+
+        function activate() { }
+    }
+})();
---
+++ app/src/dashboardController.js
@@ -0,0 +1,16 @@
+var app = angular.module("app", ['ngMaterial']);
+
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('dashboardController', dashboardController);
+
+    dashboardController.$inject = [];
+    function dashboardController() {
+        var vm = this;
+
+        // implementation goes here.
+    }
+})();
